3e4529952838a060caae7d06776861a89bc1ca96
dnnycortes <MacBook@Danys-MacBook-Pro.local>
2017-06-04 15:35:40 -0500

    Adding products view template

--- category-productos.php
+++ category-productos.php
@@ -13,16 +13,60 @@ get_header(); ?>
 			<div class="main-width">
 				<h3>Productos /</h3>
 				<h2>Tapicería</h2>
+				<div class="product">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-tapiceria-sillones.png">
+					<p>Sillones</p>
+				</div><!-- .product -->
+				<div class="product loveseats">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-tapiceria-loveseats.png">
+					<p>Love Seats</p>
+				</div><!-- .product -->
+				<div class="product sofas">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-tapiceria-sofas.png">
+					<p>Sofas</p>
+				</div><!-- .product -->
+				<div class="product salas">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-tapiceria-salas.png">
+					<p>Salas</p>
+				</div><!-- .product -->
+				<div class="product cabeceras">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-tapiceria-cabeceras.png">
+					<p>Cabeceras</p>
+				</div><!-- .product -->
+				<div class="product taburetes">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-tapiceria-taburetes.png">
+					<p>Taburetes</p>
+				</div><!-- .product -->
 			</div><!-- .main-width -->
 		</div><!-- .subcategory -->
 		<div class="subcategory carpinteria">
 			<div class="main-width">
 				<h2>Carpintería</h2>
+				<div class="product libreros">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-carpinteria-libreros.png">
+					<p>Libreros</p>
+				</div><!-- .product -->
+				<div class="product mesas">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-carpinteria-mesas.png">
+					<p>Mesas para sala</p>
+				</div><!-- .product -->
+				<div class="product recamaras">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-carpinteria-recamaras.png">
+					<p>Recámaras</p>
+				</div><!-- .product -->
 			</div><!-- .main-width -->
 		</div><!-- .subcategory -->
 		<div class="subcategory superficie">
 			<div class="main-width">
 				<h2>Superficie sólida</h2>
+				<div class="product mesas">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-carpinteria-mesas.png">
+					<p>Mesas para sala</p>
+				</div><!-- .product -->
+				<div class="product recamaras">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/icon-carpinteria-recamaras.png">
+					<p>Recámaras</p>
+				</div><!-- .product -->
 			</div><!-- .main-width -->
 		</div><!-- .subcategory -->
 		<div id="contacto">
